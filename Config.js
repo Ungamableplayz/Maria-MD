1ae2ed345771919d368fc6dde5ae504d64f0a950
Ungamableplayz <137994975+Ungamableplayz@users.noreply.github.com>
2024-04-08 23:28:36 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.ownernumber = ['+254718750167']
+global.ownername = "Ndirangu Nguni"//owner name
+global.Igname = "Ig: Ungamableplayz2"
+global.socialm = "GitHub: Ungamableplayz"
+global.location = "Kenyan"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = 'Nguni Bot 2.0' //name of the bot
 
 //sticker details
 global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
